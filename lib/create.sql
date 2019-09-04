4016eaf205f727fb1a0e41721e8bdae296b72a56
Cody Rickman <cody@creativemindstudios.com>
2019-09-04 01:11:48 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,5 +1,5 @@
-CREATE TABLE groceries (id INTEGER PRIMARY KEY, name TEXT, quantity INTEGER, aisle INTEGER);
+CREATE TABLE projects (id INTEGER PRIMARY KEY, title TEXT, category TEXT, funding_goal INTEGER, start_date TEXT, end_date TEXT);
 
-INSERT INTO groceries VALUES (1, "Apples", 2, 1);
-INSERT INTO groceries VALUES (2, "Oranges", 4, 2);
-INSERT INTO groceries VALUES(3, "Peaches", 6, 3);
+CREATE TABLE ysers (id INTEGER PRIMARY KEY, name TEXT, age INTEGER);
+
+CREATE TABLE pledges (ud INTEGER PRIMARY KEY, amount INTEGER, user_id INTEGER, project_id INTEGER);
